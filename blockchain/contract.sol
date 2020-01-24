2eda0f97106a75e47bf3c0a66c39f7b5157d5fbc
Stepan Denisov <sd.denisoff@gmail.com>
2020-01-24 12:42:26 +0300

    README update

--- blockchain/contract.sol
+++ blockchain/contract.sol
@@ -6,9 +6,18 @@ contract Ballot {
 
     constructor() public {
         admin = msg.sender;
+        countOfData = 0;
     }
+
+    mapping(uint=>int) public dataStorage;
+    uint public countOfData;
 
     function sum(int a, int b) public returns(int) {
         return a + b;
+    }
+
+    function addNumber(int a) public {
+        dataStorage[countOfData] = a;
+        countOfData++;
     }
 }
--- blockchain/contract_meta.py
+++ blockchain/contract_meta.py
@@ -1,6 +1,19 @@
 link = "https://ropsten.infura.io/v3/b35c450d1f3047a8a2799c7cabd174f7"
 
+address = "0xdff18078D98d6ab796280F7e1D89f9AB29022d35"
+
 abi = [
+    {
+        "constant": False,
+        "inputs": [
+            {"internalType": "int256", "name": "a", "type": "int256"}
+        ],
+        "name": "addNumber",
+        "outputs": [],
+        "payable": False,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
     {
         "constant": False,
         "inputs": [
@@ -9,7 +22,7 @@ abi = [
         ],
         "name": "sum",
         "outputs": [
-            {"internalType": "int256", "name": "", "type": "int256"}
+            {"internalType": "int256", "name": "b", "type": "int256"}
         ],
         "payable": False,
         "stateMutability": "nonpayable",
@@ -20,7 +33,29 @@ abi = [
         "payable": False,
         "stateMutability": "nonpayable",
         "type": "constructor"
+    },
+    {
+        "constant": True,
+        "inputs": [],
+        "name": "countOfData",
+        "outputs": [
+            {"internalType": "int256", "name": "", "type": "int256"}
+        ],
+        "payable": False,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": True,
+        "inputs": [
+            {"internalType": "int256", "name": "", "type": "int256"}
+        ],
+        "name": "dataStorage",
+        "outputs": [
+            {"internalType": "int256", "name": "", "type": "int256"}
+        ],
+        "payable": False,
+        "stateMutability": "view",
+        "type": "function"
     }
 ]
-
-address = "0x64ce1c257eA130feb1Da070E0aB0E890Ba5142Bb"
