88496ea6fc44f0ebc82e8429dd40a8b59ac848f1
Stepan Denisov <sd.denisoff@gmail.com>
2020-01-25 17:04:47 +0300

    ganache integration

--- blockchain/contract.sol
+++ blockchain/contract.sol
@@ -1,7 +1,7 @@
-pragma solidity >=0.4.22 <0.6.0;
+pragma solidity >= 0.4 .22 < 0.6 .0;
 
 
-contract Ballot {
+contract TestContract {
     address admin;
 
     constructor() public {
@@ -9,7 +9,7 @@ contract Ballot {
         countOfData = 0;
     }
 
-    mapping(uint=>int) public dataStorage;
+    mapping(uint => int) public dataStorage;
     uint public countOfData;
 
     function sum(int a, int b) public returns(int) {
@@ -19,5 +19,13 @@ contract Ballot {
     function addNumber(int a) public {
         dataStorage[countOfData] = a;
         countOfData++;
+    }
+
+    function getLastNumber() public returns(int){
+        return dataStorage[countOfData - 1];
+    }
+
+    function getCountOfData() public returns(uint) {
+        return countOfData;
     }
 }
--- blockchain/web3_interaction.py
+++ blockchain/web3_interaction.py
@@ -1,8 +1,21 @@
-import web3
-from contract_meta import *
+from web3 import Web3
+from meta import *
 
 
-web3 = web3.Web3(web3.HTTPProvider(link))
-contract = web3.eth.contract(abi=abi, address=web3.toChecksumAddress(address))
+web3 = Web3(Web3.HTTPProvider(ganache_url))
+# print(web3.isConnected())
 
-print(contract.functions.sum(2, 3).call())
+nonce = web3.eth.getTransactionCount(account_1)
+
+tx = {
+    'nonce': nonce,
+    'to': account_2,
+    'value': web3.toWei(1, 'ether'),
+    'gas': 2000000,
+    'gasPrice': web3.toWei('50', 'gwei'),
+}
+
+signed_tx = web3.eth.account.signTransaction(tx, private_key)
+tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+
+print(web3.toHex(tx_hash))
