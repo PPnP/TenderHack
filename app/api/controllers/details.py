5d1be096f943e3b7f72b82ef3fe489f1c8da7e2f
Stepan Denisov <sd.denisoff@gmail.com>
2020-01-26 12:57:00 +0300

    fixed data

--- app/api/controllers/details.py
+++ app/api/controllers/details.py
@@ -1,6 +1,6 @@
 from flask import render_template, redirect
-from flask_wtf import FlaskForm
 from flask.views import MethodView
+from datetime import datetime
 
 from app.api.models.request import Request
 from app.api.models.customer import Customer
@@ -12,7 +12,22 @@ class DetailsController(MethodView):
     action = '/details/'
 
     def get(self, id):
-        request = Request.get(Request.id == id)
+        query = Request.get(Request.id == id)
+        months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня',
+                  'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря']
+        request = {
+            'id': query.id,
+            'category': query.category,
+            'goods': query.goods,
+            'picture': query.picture,
+            'quantity': query.quantity,
+            'waiting_period': query.waiting_period.day - datetime.today().day,
+            'delivery_date': {
+                'day': query.delivery_date.day,
+                'month': months[query.delivery_date.month - 1],
+            },
+            'notes': query.notes
+        }
         return render_template('details.html', form=JoinForm(), request=request, id=id, action=self.action + id)
 
     def post(self, id):
--- app/api/controllers/search_engine.py
+++ app/api/controllers/search_engine.py
@@ -1,12 +1,30 @@
 from flask.views import MethodView
 from flask import render_template
+from datetime import datetime
 
 from app.api.models.request import Request
 
 
 class SearchEngineController(MethodView):
     def get(self, flag):
-        requests = Request.select()
+        query = Request.select()
+        requests = list()
+        for q in query:
+            months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня',
+                      'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря']
+            requests.append({
+                'id': q.id,
+                'category': q.category,
+                'goods': q.goods,
+                'picture': q.picture,
+                'quantity': q.quantity,
+                'waiting_period': q.waiting_period.day - datetime.today().day,
+                'delivery_date': {
+                    'day': q.delivery_date.day,
+                    'month': months[q.delivery_date.month - 1],
+                },
+                'notes': q.notes
+            })
         # if flag == '1':
         #     pass
         return render_template('search.html', requests=requests)
