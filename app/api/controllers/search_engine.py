1c3468b251b4878ab71232d71fd5bec2967cd392
Stepan Denisov <sd.denisoff@gmail.com>
2020-01-26 13:15:22 +0300

    adding flag

--- app/api/controllers/search_engine.py
+++ app/api/controllers/search_engine.py
@@ -7,24 +7,25 @@ from app.api.models.request import Request
 
 class SearchEngineController(MethodView):
     def get(self, flag):
-        query = Request.select()
-        requests = list()
-        for q in query:
-            months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня',
-                      'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря']
-            requests.append({
-                'id': q.id,
-                'category': q.category,
-                'goods': q.goods,
-                'picture': q.picture,
-                'quantity': q.quantity,
-                'waiting_period': q.waiting_period.day - datetime.today().day,
-                'delivery_date': {
-                    'day': q.delivery_date.day,
-                    'month': months[q.delivery_date.month - 1],
-                },
-                'notes': q.notes
-            })
-        # if flag == '1':
-        #     pass
-        return render_template('search.html', requests=requests)
+        if flag == '1':
+            query = Request.select()
+            requests = list()
+            for q in query:
+                months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня',
+                          'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря']
+                requests.append({
+                    'id': q.id,
+                    'category': q.category,
+                    'goods': q.goods,
+                    'picture': q.picture,
+                    'quantity': q.quantity,
+                    'waiting_period': q.waiting_period.day - datetime.today().day,
+                    'delivery_date': {
+                        'day': q.delivery_date.day,
+                        'month': months[q.delivery_date.month - 1],
+                    },
+                    'notes': q.notes
+                })
+            return render_template('search.html', flag=1, requests=requests)
+        else:
+            return render_template('search.html', flag=0)
